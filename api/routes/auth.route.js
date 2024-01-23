b104851e3f69bacd6390e6920ce4c11252dbc0ec
Rushikesh Patil <patilrushi136@gmail.com>
2024-01-23 17:34:09 +0530

    Created Sign Up page UI

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
 import { signup } from '../controller/auth.controller.js';
 const router = express.Router();
-router.post('/sign-up',signup)
+router.post('/signup',signup)
 export default router;
