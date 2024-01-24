80ba98b3f7033e5f01cce2c7e8a17234642c22ff
Rushikesh Patil <patilrushi136@gmail.com>
2024-01-24 20:55:34 +0530

    commmit

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,25 @@
 import { Button, Label, TextInput } from 'flowbite-react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const handleChange = (e) =>{
+    // console.log(e.target.value);
+    setFormData({...formData, [e.target.id]:e.target.value});
+  }
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    try{
+        const res = await fetch('/api/auth/sign-up',{
+          method:'POST',
+          headers:{'Content-Type':'application/json'},
+          body: JSON.stringify(formData),
+        });
+    } catch(error){
+      
+    }
+  }
   return (
     <div className='min-h-screen mt-20'>
     <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -21,18 +39,18 @@ export default function SignUp() {
       {/* right */}
 
       <div className='flex-1'>
-        <form className='flex flex-col gap-4'>
+        <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
           <div>
             <Label value='Your username' />
-            <TextInput type='text' placeholder='Username' id='username' />
+            <TextInput type='username' placeholder='Username' id='username' onChange={handleChange}/>
           </div>
           <div>
             <Label value='Your email' />
-            <TextInput type='text' placeholder='name@company.com' id='email' />
+            <TextInput type='email' placeholder='name@company.com' id='email' onChange={handleChange}/>
           </div>
           <div>
             <Label value='Your password' />
-            <TextInput type='text' placeholder='Password' id='password' />
+            <TextInput type='password' placeholder='Password' id='password' onChange={handleChange}/>
           </div>
           <Button gradientDuoTone='purpleToPink' type='submit'>
             Sign Up
